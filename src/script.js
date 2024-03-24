b624a610657614f083c0e949f82609da5deb0ee4
Pawan Sunuwar <pawanman56@gmail.com>
2024-03-25 00:47:56 +0100

    cleanup material

--- src/script.js
+++ src/script.js
@@ -40,26 +40,25 @@ const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
 
 // Materials
-const concreteMaterial = new CANNON.Material('concrete')
-const plasticMaterial = new CANNON.Material('plastic')
-const concretePlasticContactMaterial = new CANNON.ContactMaterial(
-    concreteMaterial,
-    plasticMaterial,
+const defaultMaterial = new CANNON.Material('default')
+const defaultContactMaterial = new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
     {
         friction: 0.1,
         restitution: 0.7
     }
 )
 
-world.addContactMaterial(concretePlasticContactMaterial)
+world.addContactMaterial(defaultContactMaterial)
+world.defaultContactMaterial = defaultContactMaterial
 
 // Sphere
 const sphereShape = new CANNON.Sphere(0.5)
 const sphereBody = new CANNON.Body({
     mass: 1,
     position: new CANNON.Vec3(0, 3, 0),
-    shape: sphereShape,
-    material: plasticMaterial
+    shape: sphereShape
 })
 
 world.addBody(sphereBody)
@@ -70,7 +69,7 @@ const floorBody = new CANNON.Body()
 
 floorBody.mass = 0 // mass is 0 in default; can be ignored
 floorBody.addShape(floorShape)
-floorBody.material = concreteMaterial
+floorBody.material = defaultMaterial
 floorBody.quaternion.setFromAxisAngle(
     new CANNON.Vec3(-1, 0, 0),
     Math.PI * 0.5
