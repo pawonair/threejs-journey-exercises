cddb6a452a16148b4066ac041b6ae4a0df201834
Pawan Sunuwar <pawanman56@gmail.com>
2024-03-25 00:29:50 +0100

    add cannon -> plane

--- src/script.js
+++ src/script.js
@@ -49,6 +49,20 @@ const sphereBody = new CANNON.Body({
 
 world.addBody(sphereBody)
 
+// Floor
+const floorShape = new CANNON.Plane()
+const floorBody = new CANNON.Body()
+
+floorBody.mass = 0 // mass is 0 in default; can be ignored
+floorBody.addShape(floorShape)
+floorBody.quaternion.setFromAxisAngle(
+    new CANNON.Vec3(-1, 0, 0),
+    Math.PI * 0.5
+)
+
+world.addBody(floorBody)
+
+
 /**
  * Test sphere
  */
