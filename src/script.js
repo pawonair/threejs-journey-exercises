6c3b985184a31ce9b0cbdec4538104041f50b05e
Pawan Sunuwar <pawanman56@gmail.com>
2024-03-25 01:54:23 +0100

    add reset method

--- src/script.js
+++ src/script.js
@@ -33,8 +33,22 @@ debugObject.createBox = () => {
     )
 }
 
+debugObject.reset = () => {
+    for (const object of objectsToUpdate) {
+        // Remove body
+        object.body.removeEventListener('collide', playHitSound)
+        world.removeBody(object.body)
+
+        // Remove scene
+        scene.remove(object.mesh)
+    }
+
+    objectsToUpdate.splice(0, objectsToUpdate.length)
+}
+
 gui.add(debugObject, 'createSphere')
 gui.add(debugObject, 'createBox')
+gui.add(debugObject, 'reset')
 
 /**
  * Base
