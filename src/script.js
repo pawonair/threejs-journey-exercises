34b72ff3d8ab8fbaa0c2e49d16ea877c317961a0
Pawan Sunuwar <pawanman56@gmail.com>
2024-03-25 01:31:35 +0100

    add boxes

--- src/script.js
+++ src/script.js
@@ -19,7 +19,22 @@ debugObject.createSphere = () => {
         }
     )
 }
+
+debugObject.createBox = () => {
+    createBox(
+        Math.random(),
+        Math.random(),
+        Math.random(),
+        {
+            x: (Math.random() - 0.5) * 3,
+            y: 3,
+            z: (Math.random() - 0.5) * 3
+        }
+    )
+}
+
 gui.add(debugObject, 'createSphere')
+gui.add(debugObject, 'createBox')
 
 /**
  * Base
@@ -164,11 +179,14 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Utils
  */
 const objectsToUpdate = []
+
+// Sphere
 const sphereGeometry = new THREE.SphereGeometry(1, 20, 20)
 const sphereMaterial = new THREE.MeshStandardMaterial({
     metalness: 0.3,
     roughness: 0.4,
-    envMap: environmentMapTexture
+    envMap: environmentMapTexture,
+    envMapIntensity: 0.5
 })
 
 const createSphere = (radius, position) => {
@@ -198,6 +216,42 @@ const createSphere = (radius, position) => {
 
 createSphere(0.5, { x: 0, y: 3, z:0 })
 
+// Box
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const boxMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture,
+    envMapIntensity: 0.5
+})
+
+const createBox = (width, height, depth, position) => {
+    // Three.js Mesh
+    const mesh = new THREE.Mesh(boxGeometry, boxMaterial)
+
+    mesh.scale.set(width, height, depth)
+    mesh.castShadow = true
+    mesh.position.copy(position)
+    scene.add(mesh)
+
+    // Cannon.js body
+    const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5))
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 3, 0),
+        shape,
+        material: defaultMaterial
+    })
+
+    body.position.copy(position)
+    world.addBody(body)
+    
+    // Save in objectsToUpdate
+    objectsToUpdate.push({ mesh, body})
+}
+
+createBox(1, 1.5, 2, { x: 0, y: 3, z: 0 })
+
 /**
  * Animate
  */
@@ -216,6 +270,7 @@ const tick = () =>
 
     for (const object of objectsToUpdate) {
         object.mesh.position.copy(object.body.position)
+        object.mesh.quaternion.copy(object.body.quaternion)
     }
 
     // Update controls
