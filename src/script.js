9e31cc1bfa552efaf0978952563e66e12011b8a7
Pawan Sunuwar <pawanman56@gmail.com>
2024-03-26 17:10:04 +0100

    add loader and load model

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 /**
  * Base
@@ -13,6 +14,29 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Models
+ */
+const gltfLoader = new GLTFLoader()
+
+gltfLoader.load(
+    // '/models/Duck/glTF/Duck.gltf',
+    // '/models/Duck/glTF-Binary/Duck.glb',
+    // '/models/Duck/glTF-Embedded/Duck.gltf',
+    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    (gltf) => {
+        /*while (gltf.scene.children.length) {
+            scene.add(gltf.scene.children[0])
+        }*/
+
+        const children = [ ...gltf.scene.children ]
+
+        for (const child of children) {
+            scene.add(child)
+        }
+    }
+)
 
 /**
  * Floor
