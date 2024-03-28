aeeeb1f5c04e3a5436dc59b486eac4f58bd44217
Pawan Sunuwar <pawanman56@gmail.com>
2024-03-28 23:36:33 +0100

    raycasting on mouse events

--- src/script.js
+++ src/script.js
@@ -81,6 +81,34 @@ window.addEventListener('resize', () =>
 })
 
 /**
+ * Cursor event
+ */
+const cursor = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width * 2 - 1
+    cursor.y = -(event.clientY / sizes.height) * 2 + 1
+})
+
+window.addEventListener('click', () => {
+    if (currentIntersect) {
+        switch (currentIntersect.object) {
+            case object1:
+                console.log('click on a sphere 1');
+                break;
+
+            case object2:
+                console.log('click on a sphere 2');
+                break;
+
+            case object3:
+                console.log('click on a sphere 3');
+                break;
+        }
+    }
+})
+
+/**
  * Camera
  */
 // Base camera
@@ -106,6 +134,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
+let currentIntersect = null
+
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
@@ -116,11 +146,13 @@ const tick = () =>
     object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
     // Cast a ray
-    const rayOrigin = new THREE.Vector3(-3, 0, 0)
+    /*const rayOrigin = new THREE.Vector3(-3, 0, 0)
     const rayDirection = new THREE.Vector3(1, 0, 0)
-
+    
     rayDirection.normalize()
-    raycaster.set(rayOrigin, rayDirection)
+    raycaster.set(rayOrigin, rayDirection)*/
+    
+    raycaster.setFromCamera(cursor, camera)
 
     const objectsToTest = [object1, object2, object3]
     const intersects = raycaster.intersectObjects(objectsToTest)
@@ -131,6 +163,19 @@ const tick = () =>
 
     for (const intersect of intersects) {
         intersect.object.material.color.set('#0000ff')
+    }
+
+    if (intersects.length) {
+        if (currentIntersect === null) {
+            console.log('mouseenter');
+        }
+        currentIntersect = intersects[0]
+
+    } else {
+        if (currentIntersect) {
+            console.log('leave');
+        }
+        currentIntersect = null
     }
 
     // Update controls
