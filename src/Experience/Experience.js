c3fd30cc0c63ad11f87a58feda4374ca4da40e0e
Pawan Sunuwar <pawanman56@gmail.com>
2024-04-03 15:59:57 +0200

    refactor classes + event-emitter

---
+++ src/Experience/Experience.js
@@ -0,0 +1,28 @@
+import Sizes from './Utils/Sizes.js'
+import Time from './Utils/Time.js'
+
+export default class Experience {
+    constructor (canvas) {
+        // Global access
+        window.experience = this
+
+        // Options
+        this.canvas = canvas
+
+        // Setup
+        this.sizes = new Sizes()
+        this.time = new Time()
+
+        // Sizes resize event
+        this.sizes.on('resize', () => this.resize())
+
+        // Time tick event
+        this.time.on('tick', () => this.update())
+    }
+
+    resize () {
+        console.log(this);
+    }
+
+    update () {}
+}
---
+++ src/test.js
@@ -0,0 +1,9 @@
+const oneThing = {
+    hello: 'module'
+}
+
+const anotherThing = () => {
+    console.log('Hello modules');
+}
+
+export { oneThing, anotherThing }
