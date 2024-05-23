31b916c86f8b4238d1e98617ec93b179253f489d
Pawan Sunuwar <pawanman56@gmail.com>
2024-05-23 15:32:41 +0200

    handle the size

--- src/shaders/galaxy/vertex.glsl
+++ src/shaders/galaxy/vertex.glsl
@@ -1,14 +1,18 @@
+uniform float uSize;
+
+attribute float aScale;
+
 void main() {
     /**
-    * Position
-    */
+     * Position
+     */
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
 
     /**
-    * Size
-    */
-    gl_PointSize = 2.0;
+     * Size
+     */
+    gl_PointSize = aScale * uSize;
 }
