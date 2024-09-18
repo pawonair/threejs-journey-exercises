cfa4f009d82de7f9de238cf03f9fd6189d5e0a20
Pawan Sunuwar <pawanman56@gmail.com>
2024-09-18 21:00:07 +0200

    Add fragment shader

--- src/script.js
+++ src/script.js
@@ -89,11 +89,22 @@ const smokeGeometry = new THREE.PlaneGeometry(1, 1, 16, 64)
 smokeGeometry.translate(0, 0.5, 0)
 smokeGeometry.scale(1.5, 6, 1.5)
 
+// Perlin texture
+const perlinTexture = textureLoader.load('./perlin.png')
+perlinTexture.wrapS = THREE.RepeatWrapping
+perlinTexture.wrapT = THREE.RepeatWrapping
+
 // Material
 const smokeMaterial = new THREE.ShaderMaterial({
     vertexShader: coffeeSmokeVertexShader,
     fragmentShader: coffeeSmokeFragmentShader,
     side: THREE.DoubleSide,
+    uniforms: {
+        uTime: new THREE.Uniform(0),
+        uPerlinTexture: new THREE.Uniform(perlinTexture)
+    },
+    side: THREE.DoubleSide,
+    transparent: true,
     // wireframe: true
 })
 
@@ -110,6 +121,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update smoke
+    smokeMaterial.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
--- src/shaders/coffeeSmoke/vertex.glsl
+++ src/shaders/coffeeSmoke/vertex.glsl
@@ -1,4 +1,9 @@
+varying vec2 vUv;
+
 void main() {
     // final position
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+    // Varyings
+    vUv = uv;
 }
