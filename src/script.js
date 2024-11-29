dd10de3ca5ca214ab4473685d6ec6f60ca9ce453
Pawan Sunuwar <pawanman56@gmail.com>
2024-11-29 22:41:02 +0100

    add gui color

--- src/script.js
+++ src/script.js
@@ -79,11 +79,21 @@ gui
 /**
  * Material
  */
+const materialParameters = {}
+materialParameters.color = '#70c1ff'
+
+gui
+    .addColor(materialParameters, 'color')
+    .onChange(() => {
+        material.uniforms.uColor.value.set(materialParameters.color)
+    })
+
 const material = new THREE.ShaderMaterial({
     vertexShader: holographicVertexShader,
     fragmentShader: holographicFragmentShader,
     uniforms: {
         uTime: new THREE.Uniform(0),
+        uColor: new THREE.Uniform(new THREE.Color(materialParameters.color)),
     },
     transparent: true,
     side: THREE.DoubleSide,
