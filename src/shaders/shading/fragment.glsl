f60b67b2f62e9bf7ce4b9a35c5b75a41e61c5897
Pawan Sunuwar <pawanman56@gmail.com>
2025-04-01 17:13:50 +0200

    add directionalLight

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,16 +1,30 @@
 uniform vec3 uColor;
 
+varying vec3 vNormal;
+varying vec3 vPosition;
+
 #include ../includes/ambientLight.glsl
+#include ../includes/directionalLight.glsl
 
 void main()
 {
+    vec3 normal = normalize(vNormal);
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 color = uColor;
 
     // Light
     vec3 light = vec3(0.0);
     light += ambientLight(
         vec3(1.0), // Light color
-        0.2 // Light intensity
+        0.03 // Light intensity
+    );
+    light +=directionalLight(
+        vec3(0.1, 0.1, 1.0), // Light color
+        0.5, // Light intensity
+        normal, // Normal
+        vec3(0.0, 0.0, 3.0), // Light position
+        viewDirection, // View direction
+        20.0 // Specular power
     );
     color *= light;
 
