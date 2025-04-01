9b82c35e9bca947a70d93b663b5f6971b71b8c89
Pawan Sunuwar <pawanman56@gmail.com>
2025-04-01 17:28:29 +0200

    add pointLight

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -5,6 +5,7 @@ varying vec3 vPosition;
 
 #include ../includes/ambientLight.glsl
 #include ../includes/directionalLight.glsl
+#include ../includes/pointLight.glsl
 
 void main()
 {
@@ -16,15 +17,25 @@ void main()
     vec3 light = vec3(0.0);
     light += ambientLight(
         vec3(1.0), // Light color
-        0.03 // Light intensity
+        0.03       // Light intensity
     );
-    light +=directionalLight(
+    light += directionalLight(
         vec3(0.1, 0.1, 1.0), // Light color
-        0.5, // Light intensity
-        normal, // Normal
+        1.0,                 // Light intensity
+        normal,              // Normal
         vec3(0.0, 0.0, 3.0), // Light position
-        viewDirection, // View direction
-        20.0 // Specular power
+        viewDirection,       // View direction
+        20.0                 // Specular power
+    );
+    light += pointLight(
+        vec3(1.0, 0.1, 0.1), // Light color
+        1.0,                 // Light intensity
+        normal,              // Normal
+        vec3(0.0, 2.5, 0.0), // Light position
+        viewDirection,       // View direction
+        20.0,                // Specular power
+        vPosition,           // Position
+        0.25                 // Light decay
     );
     color *= light;
 
