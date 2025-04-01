2e76955540df3800fc9b2dc39983f8a13582230c
Pawan Sunuwar <pawanman56@gmail.com>
2025-04-01 16:28:42 +0200

    init ambientLight

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,8 +1,18 @@
 uniform vec3 uColor;
 
+#include ../includes/ambientLight.glsl
+
 void main()
 {
     vec3 color = uColor;
+
+    // Light
+    vec3 light = vec3(0.0);
+    light += ambientLight(
+        vec3(1.0), // Light color
+        0.2 // Light intensity
+    );
+    color *= light;
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
