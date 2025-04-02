f582db703ea12f739bb3bad3c2d00b37a2599d40
Pawan Sunuwar <pawanman56@gmail.com>
2025-04-02 17:17:59 +0200

    touchup

--- src/script.js
+++ src/script.js
@@ -18,15 +18,17 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Axes helper
-const axesHelper = new THREE.AxesHelper()
+/* const axesHelper = new THREE.AxesHelper()
 axesHelper.position.y += 0.25
-scene.add(axesHelper)
+scene.add(axesHelper) */
 
 /**
  * Water
  */
 // Geometry
 const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512)
+waterGeometry.deleteAttribute('normal')
+waterGeometry.deleteAttribute('uv')
 
 // Colors
 debugObject.depthColor = '#ff4000'
@@ -118,6 +120,7 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+renderer.toneMapping = THREE.ACESFilmicToneMapping
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
