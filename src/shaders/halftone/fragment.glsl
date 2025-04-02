5b410d350123b385c527f779d5cec51a4bc1c411
Pawan Sunuwar <pawanman56@gmail.com>
2025-04-02 19:22:14 +0200

    add halftone points

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -34,13 +34,26 @@ void main()
 
     // Halftone
     float repetitions = 50.0;
+    vec3 direction = vec3(0.0, -1.0, 0.0);
+    float low = -0.8;
+    float high = 1.5;
+
+    vec3 pointColor = vec3(1.0, 0.0, 0.0);
+
+    float intensity = dot(normal, direction);
+    intensity = smoothstep(low, high, intensity);
 
     vec2 uv = gl_FragCoord.xy / uResolution.y;
     uv *= repetitions;
     uv = mod(uv, 1.0);
 
+    float point = distance(uv, vec2(0.5));
+    point = 1.0 - step(0.5 * intensity, point);
+
+    color = mix(color, pointColor, point);
+
     // Final color
-    gl_FragColor = vec4(uv, 1.0, 1.0);
+    gl_FragColor = vec4(color, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
