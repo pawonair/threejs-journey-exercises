0b01b2457337616c33f4370838acfa2e54df834a
Pawan Sunuwar <pawanman56@gmail.com>
2025-04-02 19:10:15 +0200

    add uv grids

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -1,7 +1,11 @@
 uniform vec3 uColor;
+uniform vec2 uResolution;
 
 varying vec3 vNormal;
 varying vec3 vPosition;
+
+#include ../includes/ambientLight.glsl
+#include ../includes/directionalLight.glsl
 
 void main()
 {
@@ -9,8 +13,34 @@ void main()
     vec3 normal = normalize(vNormal);
     vec3 color = uColor;
 
+    // Lights
+    vec3 light = vec3(0.0);
+
+    light += ambientLight(
+        vec3(1.0), // Light color
+        1.0        // Light intensity
+    );
+
+    light += directionalLight(
+        vec3(1.0, 1.0, 1.0), // Light color
+        1.0,                 // Light intensity
+        normal,              // Normal
+        vec3(1.0, 1.0, 0.0), // Light direction
+        viewDirection,       // View direction
+        1.0                  // Light distance
+    );
+
+    color *= light;
+
+    // Halftone
+    float repetitions = 50.0;
+
+    vec2 uv = gl_FragCoord.xy / uResolution.y;
+    uv *= repetitions;
+    uv = mod(uv, 1.0);
+
     // Final color
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(uv, 1.0, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
