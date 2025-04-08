e3ccbdd03675738efb57bed999cb9741dbda9505
Pawan Sunuwar <pawanman56@gmail.com>
2025-04-09 00:36:40 +0200

    load texture + add textures mix

--- src/shaders/earth/fragment.glsl
+++ src/shaders/earth/fragment.glsl
@@ -1,3 +1,7 @@
+uniform sampler2D uDayTexture;
+uniform sampler2D uNightTexture;
+uniform sampler2D uSpecularCloudsTexture;
+
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vPosition;
@@ -6,7 +10,18 @@ void main()
 {
     vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 normal = normalize(vNormal);
-    vec3 color = vec3(vUv, 1.0);
+    vec3 color = vec3(0.0);
+
+    // Sun orientation
+    vec3 uSunDirection = vec3(0.0, 0.0, 1.0);
+    float sunOrientation = dot(uSunDirection, normal);
+    color = vec3(sunOrientation);
+
+    // Day & night color
+    float dayMix = smoothstep(-0.25, 0.5, sunOrientation);
+    vec3 dayColor = texture(uDayTexture, vUv).rgb;
+    vec3 nightColor = texture(uNightTexture, vUv).rgb;
+    color = mix(nightColor, dayColor, dayMix);
 
     // Final color
     gl_FragColor = vec4(color, 1.0);
