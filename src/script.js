051b6dd4a51e8352b6a2217e7859199483013a11
Pawan Sunuwar <pawanman56@gmail.com>
2025-05-03 16:17:36 +0200

    mix positions + simplex-noise transition

--- src/script.js
+++ src/script.js
@@ -128,6 +128,7 @@ gltfLoader.load('./models.glb', (gltf) =>
     // Geometry
     particles.geometry = new THREE.BufferGeometry()
     particles.geometry.setAttribute('position', particles.positions[1])
+    particles.geometry.setAttribute('aPositionTarget', particles.positions[3])
 
     // Material
     particles.material = new THREE.ShaderMaterial({
@@ -136,7 +137,8 @@ gltfLoader.load('./models.glb', (gltf) =>
         uniforms:
         {
             uSize: new THREE.Uniform(0.2),
-            uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio))
+            uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
+            uProgress: new THREE.Uniform(0)
         },
         blending: THREE.AdditiveBlending,
         depthWrite: false
@@ -145,6 +147,14 @@ gltfLoader.load('./models.glb', (gltf) =>
     // Points
     particles.points = new THREE.Points(particles.geometry, particles.material)
     scene.add(particles.points)
+
+    // Tweaks
+    gui
+        .add(particles.material.uniforms.uProgress, 'value')
+        .min(0)
+        .max(1)
+        .step(0.001)
+        .name('uProgress')
 })
 
 /**
