e637a278f984b586aceb871e69a2817763600a49
Pawan Sunuwar <pawanman56@gmail.com>
2025-05-03 16:30:47 +0200

    add random particle sizes

--- src/script.js
+++ src/script.js
@@ -127,9 +127,15 @@ gltfLoader.load('./models.glb', (gltf) =>
     }
 
     // Geometry
+    const sizesArray = new Float32Array(particles.maxCount)
+
+    for (let i = 0; i < particles.maxCount; i++)
+        sizesArray[i] = Math.random();
+
     particles.geometry = new THREE.BufferGeometry()
     particles.geometry.setAttribute('position', particles.positions[particles.index])
     particles.geometry.setAttribute('aPositionTarget', particles.positions[3])
+    particles.geometry.setAttribute('aSize', new THREE.BufferAttribute(sizesArray, 1))
 
     // Material
     particles.material = new THREE.ShaderMaterial({
@@ -137,7 +143,7 @@ gltfLoader.load('./models.glb', (gltf) =>
         fragmentShader: particlesFragmentShader,
         uniforms:
         {
-            uSize: new THREE.Uniform(0.2),
+            uSize: new THREE.Uniform(0.4),
             uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
             uProgress: new THREE.Uniform(0)
         },
