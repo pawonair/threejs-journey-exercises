f199ef308956322deecd3c9c49f40bf45cdbfc1a
Pawan Sunuwar <pawanman56@gmail.com>
2025-06-01 20:09:36 +0200

    update life of particles

--- src/script.js
+++ src/script.js
@@ -112,7 +112,7 @@ for (let i = 0; i < baseGeometry.count; i++)
     baseParticlesTexture.image.data[i4 + 0] = baseGeometry.instance.attributes.position.array[i3 + 0]
     baseParticlesTexture.image.data[i4 + 1] = baseGeometry.instance.attributes.position.array[i3 + 1]
     baseParticlesTexture.image.data[i4 + 2] = baseGeometry.instance.attributes.position.array[i3 + 2]
-    baseParticlesTexture.image.data[i4 + 3] = 0
+    baseParticlesTexture.image.data[i4 + 3] = Math.random()
 }
 
 // Particles variable
@@ -121,6 +121,8 @@ gpgpu.computation.setVariableDependencies(gpgpu.particlesVariable, [ gpgpu.parti
 
 // Uniforms
 gpgpu.particlesVariable.material.uniforms.uTime = new THREE.Uniform(0)
+gpgpu.particlesVariable.material.uniforms.uDeltaTime = new THREE.Uniform(0)
+gpgpu.particlesVariable.material.uniforms.uBase = new THREE.Uniform(baseParticlesTexture)
 
 // Init
 gpgpu.computation.init()
@@ -206,6 +208,7 @@ const tick = () =>
 
     // GPGPU Update
     gpgpu.particlesVariable.material.uniforms.uTime.value = elapsedTime
+    gpgpu.particlesVariable.material.uniforms.uDeltaTime.value = deltaTime
     gpgpu.computation.compute()
     particles.material.uniforms.uParticlesTexture.value = gpgpu.computation.getCurrentRenderTarget(gpgpu.particlesVariable).texture
 
