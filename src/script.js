6741acf6022a3edd2dd9022fc54829ecb67f687a
Pawan Sunuwar <pawanman56@gmail.com>
2025-06-01 20:30:51 +0200

    initial setup

--- src/script.js
+++ src/script.js
@@ -1,11 +1,17 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import particlesVertexShader from './shaders/particles/vertex.glsl'
-import particlesFragmentShader from './shaders/particles/fragment.glsl'
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
+import GUI from 'lil-gui'
 
 /**
  * Base
  */
+// Debug
+const gui = new GUI({ width: 325 })
+const debugObject = {}
+
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -13,7 +19,78 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Loaders
-const textureLoader = new THREE.TextureLoader()
+const rgbeLoader = new RGBELoader()
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('./draco/')
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+/**
+ * Environment map
+ */
+rgbeLoader.load('./urban_alley_01_1k.hdr', (environmentMap) =>
+{
+    environmentMap.mapping = THREE.EquirectangularReflectionMapping
+
+    scene.background = environmentMap
+    scene.environment = environmentMap
+})
+
+/**
+ * Wobble
+ */
+// Material
+const material = new THREE.MeshPhysicalMaterial({
+    metalness: 0,
+    roughness: 0.5,
+    color: '#ffffff',
+    transmission: 0,
+    ior: 1.5,
+    thickness: 1.5,
+    transparent: true,
+    wireframe: false
+})
+
+// Tweaks
+gui.add(material, 'metalness', 0, 1, 0.001)
+gui.add(material, 'roughness', 0, 1, 0.001)
+gui.add(material, 'transmission', 0, 1, 0.001)
+gui.add(material, 'ior', 0, 10, 0.001)
+gui.add(material, 'thickness', 0, 10, 0.001)
+gui.addColor(material, 'color')
+
+// Geometry
+const geometry = new THREE.IcosahedronGeometry(2.5, 50)
+
+// Mesh
+const wobble = new THREE.Mesh(geometry, material)
+wobble.receiveShadow = true
+wobble.castShadow = true
+scene.add(wobble)
+
+/**
+ * Plane
+ */
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(15, 15, 15),
+    new THREE.MeshStandardMaterial()
+)
+plane.receiveShadow = true
+plane.rotation.y = Math.PI
+plane.position.y = - 5
+plane.position.z = 5
+scene.add(plane)
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.normalBias = 0.05
+directionalLight.position.set(0.25, 2, - 2.25)
+scene.add(directionalLight)
 
 /**
  * Sizes
@@ -31,9 +108,6 @@ window.addEventListener('resize', () =>
     sizes.height = window.innerHeight
     sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
-    // Materials
-    particlesMaterial.uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
-
     // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
@@ -48,7 +122,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0, 0, 18)
+camera.position.set(13, - 3, - 5)
 scene.add(camera)
 
 // Controls
@@ -62,138 +136,24 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     antialias: true
 })
-renderer.setClearColor('#181818')
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 1
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(sizes.pixelRatio)
 
 /**
- * Displacement
- */
-const displacement = {}
-
-// 2D Canvas
-displacement.canvas = document.createElement('canvas')
-displacement.canvas.width = 128
-displacement.canvas.height = 128
-displacement.canvas.style.position = 'fixed'
-displacement.canvas.style.width = '256px'
-displacement.canvas.style.height = '256px'
-displacement.canvas.style.top = 0
-displacement.canvas.style.left = 0
-displacement.canvas.style.zIndex = 10
-document.body.append(displacement.canvas)
-
-// Context
-displacement.context = displacement.canvas.getContext('2d')
-displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height)
-
-// Glow Image
-displacement.glowImage = new Image()
-displacement.glowImage.src = './glow.png'
-
-// Interactive plane
-displacement.interactivePlane = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshBasicMaterial({ color: 'red', side: THREE.DoubleSide })
-)
-displacement.interactivePlane.visible = false
-scene.add(displacement.interactivePlane)
-
-// Raycaster
-displacement.raycaster = new THREE.Raycaster()
-
-// Coordinates
-displacement.screenCursor = new THREE.Vector2(9999, 9999)
-displacement.canvasCursor = new THREE.Vector2(9999, 9999)
-displacement.canvasCursorPrevious = new THREE.Vector2(9999, 9999)
-
-window.addEventListener('pointermove', (event) =>
-{
-    displacement.screenCursor.x = (event.clientX / sizes.width) * 2 - 1
-    displacement.screenCursor.y = (event.clientY / sizes.height) * 2 - 1
-})
-
-// Texture
-displacement.texture = new THREE.CanvasTexture(displacement.canvas)
-
-/**
- * Particles
- */
-const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128)
-particlesGeometry.setIndex (null)
-particlesGeometry.deleteAttribute('normal')
-
-const intensitiesArray = new Float32Array(particlesGeometry.attributes.position.count)
-const anglesArray = new Float32Array(particlesGeometry.attributes.position.count)
-
-for (let i = 0; i < particlesGeometry.attributes.position.count; i++) {
-    intensitiesArray[i] = Math.random()
-    anglesArray[i] = Math.random() * Math.PI * 2
-}
-
-particlesGeometry.setAttribute('aIntensity', new THREE.BufferAttribute(intensitiesArray, 1))
-particlesGeometry.setAttribute('aAngle', new THREE.BufferAttribute(anglesArray, 1))
-
-const particlesMaterial = new THREE.ShaderMaterial({
-    vertexShader: particlesVertexShader,
-    fragmentShader: particlesFragmentShader,
-    uniforms:
-    {
-        uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
-        uPictureTexture: new THREE.Uniform(textureLoader.load('./picture-1.png')),
-        uDisplacementTexture: new THREE.Uniform(displacement.texture),
-    }
-})
-const particles = new THREE.Points(particlesGeometry, particlesMaterial)
-scene.add(particles)
-
-/**
  * Animate
  */
+const clock = new THREE.Clock()
+
 const tick = () =>
 {
+    const elapsedTime = clock.getElapsedTime()
+
     // Update controls
     controls.update()
-
-    /**
-     * Raycaster
-     */
-    displacement.raycaster.setFromCamera(displacement.screenCursor, camera)
-    const intersections = displacement.raycaster.intersectObject(displacement.interactivePlane)
-
-    if (intersections.length) {
-        const uv = intersections[0].uv
-
-        displacement.canvasCursor.x = uv.x * displacement.canvas.width
-        displacement.canvasCursor.y = uv.y * displacement.canvas.height
-    }
-
-    /**
-     * Displacement
-     */
-    displacement.context.globalCompositeOperation = 'source-over'
-    displacement.context.globalAlpha = 0.02
-    displacement.context.fillRect(0,0, displacement.canvas.width, displacement.canvas.height)
-
-    // Speed alpha
-    const cursorDistance = displacement.canvasCursorPrevious.distanceTo(displacement.canvasCursor)
-    displacement.canvasCursorPrevious.copy(displacement.canvasCursor)
-    const alpha = Math.min(cursorDistance * 0.1, 1)
-
-    // Draw glow
-    const glowSize = displacement.canvas.width * 0.25
-    displacement.context.globalCompositeOperation = 'lighten'
-    displacement.context.globalAlpha = alpha
-    displacement.context.drawImage(
-        displacement.glowImage,
-        displacement.canvasCursor.x - glowSize * 0.5,
-        displacement.canvasCursor.y - glowSize * 0.5,
-        glowSize,
-        glowSize
-    )
-
-    // Texture
-    displacement.texture.needsUpdate = true
 
     // Render
     renderer.render(scene, camera)
